c2db887542e7e70c6e69e0ca5fef1423ba837898
Douglas Motta <tibeca@gmail.com>
2021-10-21 21:38:37 +0100

    Cria authorization interceptor

---
+++ core/src/main/java/com/example/core/data/network/interceptor/AuthorizationInterceptor.kt
@@ -0,0 +1,46 @@
+package com.example.core.data.network.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.util.*
+
+class AuthorizationInterceptor(
+    private val publicKey: String,
+    private val privateKey: String,
+    private val calendar: Calendar
+) : Interceptor {
+
+    @Suppress("MagicNumber")
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val requestUrl = request.url
+
+        val ts = (calendar.timeInMillis / 1000L).toString() // time in seconds
+        val hash = "$ts$privateKey$publicKey".md5()
+        val newUrl = requestUrl.newBuilder()
+            .addQueryParameter(QUERY_PARAMETER_TS, ts)
+            .addQueryParameter(QUERY_PARAMETER_API_KEY, publicKey)
+            .addQueryParameter(QUERY_PARAMETER_HASH, hash)
+            .build()
+
+        return chain.proceed(
+            request.newBuilder()
+                .url(newUrl)
+                .build()
+        )
+    }
+
+    @Suppress("MagicNumber")
+    private fun String.md5(): String {
+        val md = MessageDigest.getInstance("MD5")
+        return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
+    }
+
+    companion object {
+        private const val QUERY_PARAMETER_TS = "ts"
+        private const val QUERY_PARAMETER_API_KEY = "apikey"
+        private const val QUERY_PARAMETER_HASH = "hash"
+    }
+}
