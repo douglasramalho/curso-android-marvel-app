4c14c974054e3e05813e22b9eab7a756e9e07bfd
Douglas Motta <tibeca@gmail.com>
2021-11-03 20:52:20 +0000

    Melhora organização do projeto

---
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.marvelapp.presentation.characters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.core.domain.model.Character
+
+class CharactersAdapter : ListAdapter<Character, CharactersViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharactersViewHolder {
+        return CharactersViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: CharactersViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Character>() {
+            override fun areItemsTheSame(
+                oldItem: Character,
+                newItem: Character
+            ): Boolean {
+                return oldItem.name == newItem.name
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Character,
+                newItem: Character
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
