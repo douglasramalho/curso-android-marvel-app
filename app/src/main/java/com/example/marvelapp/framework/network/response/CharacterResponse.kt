c3b1f1f96dca92c2a0cd5aef8b8ec279c7e39d37
Douglas Motta <tibeca@gmail.com>
2021-11-07 02:30:31 +0000

    Exibe personagens da api da Marvel

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -12,5 +12,6 @@ fun CharacterResponse.toCharacterModel(): Character {
     return Character(
         name = this.name,
         imageUrl = "${this.thumbnail.path}.${this.thumbnail.extension}"
+            .replace("http", "https")
     )
 }
--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,15 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.core.domain.model.Character
 import com.example.marvelapp.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CharactersFragment : Fragment() {
 
     private var _binding: FragmentCharactersBinding? = null
     private val binding: FragmentCharactersBinding get() = _binding!!
+
+    private val viewModel: CharactersViewModel by viewModels()
 
     private val charactersAdapter = CharactersAdapter()
 
@@ -32,38 +38,11 @@ class CharactersFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initCharactersAdapter()
 
-        charactersAdapter.submitList(
-            listOf(
-                Character(
-                    "3D-Man",
-                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
-                ),
-                Character(
-                    "3D-Man",
-                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
-                ),
-                Character(
-                    "3D-Man",
-                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
-                ),
-                Character(
-                    "3D-Man",
-                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
-                ),
-                Character(
-                    "3D-Man",
-                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
-                ),
-                Character(
-                    "3D-Man",
-                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
-                ),
-                Character(
-                    "3D-Man",
-                    "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
-                ),
-            )
-        )
+        lifecycleScope.launch {
+            viewModel.charactersPagingData("").collect { pagingData ->
+                charactersAdapter.submitData(pagingData)
+            }
+        }
     }
 
     private fun initCharactersAdapter() {
