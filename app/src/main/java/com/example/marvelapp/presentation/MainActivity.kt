50e72b8cc682bb462ffa35ed55e5915a3c1bae9d
Joselaine Aparecida <joselaine.aparecida@gmail.com>
2021-12-08 11:52:43 -0300

    config layout app

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.example.marvelapp
+package com.example.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.marvelapp.R
 import com.example.marvelapp.databinding.ActivityMainBinding
 
 
@@ -20,8 +22,20 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_container) as NavHostFragment
         setContentView(binding.root)
         navController = navHostFragment.navController
+
         appBarConfiguration = AppBarConfiguration(
-            setOf(),
+            setOf(R.id.charactersFragment, R.id.favoritesFragment, R.id.aboutFragment),
         )
+
+        binding.bottomNavMain.setupWithNavController(navController)
+        binding.toolbarApp.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val isTopLevelDestination =
+                appBarConfiguration.topLevelDestinations.contains(destination.id)
+            if (!isTopLevelDestination) {
+                binding.toolbarApp.setNavigationIcon(R.drawable.ic_back)
+            }
+        }
     }
 }
