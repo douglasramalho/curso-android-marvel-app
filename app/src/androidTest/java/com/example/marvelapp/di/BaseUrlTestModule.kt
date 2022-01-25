e31ffedffa5ae83a9347536c54b7f014a8078bfc
Douglas Pessoal <me@MBP-de-DOUGLAS.lan>
2022-01-25 19:22:17 +0000

    Implementa testes de interface com Espresso

---
+++ app/src/androidTest/java/com/example/marvelapp/di/BaseUrlTestModule.kt
@@ -0,0 +1,16 @@
+package com.example.marvelapp.di
+
+import com.example.marvelapp.framework.di.qualifier.BaseUrl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object BaseUrlTestModule {
+
+    @BaseUrl
+    @Provides
+    fun provideBaseUrl(): String = "http://localhost:8080/"
+}
