f0b4b0264ba9cc9941d9484fa51e463209ca1b60
Douglas Motta <douglas1102rm@hotmail.com>
2022-02-04 21:46:07 +0000

    Atualiza dependências

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,9 +1,6 @@
 package com.example.marvelapp.presentation.characters
 
-import android.view.View
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
