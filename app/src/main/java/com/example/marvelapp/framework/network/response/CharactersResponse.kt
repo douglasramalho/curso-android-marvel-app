337083fe30c2ae13bdb1e676511c817e9ec5d460
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2022-02-11 11:10:37 -0300

    implementation paging

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharactersResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharactersResponse.kt
@@ -1,7 +1,16 @@
 package com.example.marvelapp.framework.network.response
 
+import br.com.nicolas.core.domain.model.Character
+
 data class CharactersResponse(
     val id: String,
     val name: String,
     val thumbnail: ThumbnailResponse
 )
+
+fun CharactersResponse.toCharacterModel(): Character {
+    return Character(
+        name = this.name,
+        imageUrl = "${this.thumbnail.path}.${thumbnail.extension}"
+    )
+}
