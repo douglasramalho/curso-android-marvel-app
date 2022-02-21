66c615daf91370004f21126f4eba6ce548cc8854
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-02-20 21:39:33 -0300

    feat: implementa CharactersViewHolder e CharacterAdapter

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.marvelapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
 
     override fun onCreateView(
