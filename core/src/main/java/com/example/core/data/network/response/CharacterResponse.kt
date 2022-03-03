3336c6089d34cbcfcffb5174f75aa3f8c1b81e01
Joselaine <joselaine.aparecida@gmail.com>
2022-03-03 09:50:22 -0300

    mapeamento dos objetos de response da api Marvel

---
+++ core/src/main/java/com/example/core/data/network/response/CharacterResponse.kt
@@ -0,0 +1,7 @@
+package com.example.core.data.network.response
+
+data class CharacterResponse(
+    val id: String,
+    val nome: String,
+    val thumbnail: ThumbnailResponse,
+)
---
+++ core/src/main/java/com/example/core/data/network/response/ThumbnailResponse.kt
@@ -0,0 +1,7 @@
+package com.example.core.data.network.response
+
+data class ThumbnailResponse(
+    val path: String,
+    val extension: String,
+
+)
