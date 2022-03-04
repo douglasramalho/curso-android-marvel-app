645bff80cdb163b0f08e3715f7ba1e62047f9623
Vitor Walcker <vitor0321@hotmail.com>
2022-03-03 22:01:40 -0300

    implementando launchFragmentInHiltContainer

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,21 +1,28 @@
 package com.example.marvelapp.presentation.characters
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.marvelapp.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class CharactersFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Before
     fun setUp() {
-        launchFragmentInContainer<CharactersFragment>()
+        launchFragmentInHiltContainer<CharactersFragment>()
     }
 
     @Test
-    fun shouldShowCharacters_whenViewIsCreated(){
+    fun shouldShowCharacters_whenViewIsCreated() {
 
     }
 }
