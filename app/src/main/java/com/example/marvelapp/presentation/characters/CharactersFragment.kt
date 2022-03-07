2f86c6044d05eb74e19a050fef78d6356d06b7c4
Joselaine <joselaine.aparecida@gmail.com>
2022-03-07 11:18:07 -0300

    Criado o RecyclerView de personagens

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,15 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.core.domain.model.Character
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding: FragmentCharactersBinding get() = _binding!!
+
+    private val charactersAdapter = CharactersAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ) = FragmentCharactersBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+
+        charactersAdapter.submitList(
+            listOf()
+        )
+    }
+
+    private fun initCharactersAdapter() {
+        with(binding.recyclerCharacters){
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
     }
 }
