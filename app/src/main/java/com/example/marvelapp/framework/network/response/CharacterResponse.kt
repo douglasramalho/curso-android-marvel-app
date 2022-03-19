cf5f57d0bef1277cefbc69491e46607853468220
Joselaine <joselaine.aparecida@gmail.com>
2022-03-19 19:01:09 -0300

    cria pagingSource e DI para Repository e DataSource

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,7 +1,16 @@
 package com.example.marvelapp.framework.network.response
 
+import com.example.core.domain.model.Character
+
 data class CharacterResponse(
     val id: String,
     val nome: String,
     val thumbnail: ThumbnailResponse,
 )
+
+fun CharacterResponse.toCharacterModel(): Character{
+    return Character(
+        name = this.nome,
+        imageUrl = "${this.thumbnail.path}.${this.thumbnail.extension}"
+    )
+}
