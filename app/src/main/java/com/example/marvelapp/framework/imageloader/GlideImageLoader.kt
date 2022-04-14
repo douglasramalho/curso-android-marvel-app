fe63119db2d5bed830017f685f1e3f24788677bd
Vitor Walcker <vitor0321@hotmail.com>
2022-04-13 21:35:59 -0300

    add detail fragment

---
+++ app/src/main/java/com/example/marvelapp/framework/imageloader/GlideImageLoader.kt
@@ -0,0 +1,17 @@
+package com.example.marvelapp.framework.imageloader
+
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import com.bumptech.glide.Glide
+import javax.inject.Inject
+
+
+class GlideImageLoader @Inject constructor() : ImageLoader {
+
+    override fun load(imageView: ImageView, imageUrl: String, @DrawableRes fallback: Int) {
+        Glide.with(imageView.rootView)
+            .load(imageUrl)
+            .fallback(fallback)
+            .into(imageView)
+    }
+}
---
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.marvelapp.presentation.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel()
