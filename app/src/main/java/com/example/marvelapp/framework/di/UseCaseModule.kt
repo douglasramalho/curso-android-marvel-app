bc4169436cc0db8d627f0c58515384187f40cd67
Douglas Motta <douglas1102rm@hotmail.com>
2022-04-25 19:31:23 +0100

    Exibe os favoritos na tela de favoritos

--- app/src/main/java/com/example/marvelapp/framework/di/UseCaseModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/UseCaseModule.kt
@@ -8,6 +8,8 @@ import com.example.core.usecase.AddFavoriteUseCase
 import com.example.core.usecase.AddFavoriteUseCaseImpl
 import com.example.core.usecase.CheckFavoriteUseCase
 import com.example.core.usecase.CheckFavoriteUseCaseImpl
+import com.example.core.usecase.GetFavoritesUseCase
+import com.example.core.usecase.GetFavoritesUseCaseImpl
 import com.example.core.usecase.RemoveFavoriteUseCase
 import com.example.core.usecase.RemoveFavoriteUseCaseImpl
 import dagger.Binds
@@ -35,4 +37,7 @@ interface UseCaseModule {
 
     @Binds
     fun bindRemoveFavoriteUseCase(useCase: RemoveFavoriteUseCaseImpl): RemoveFavoriteUseCase
+
+    @Binds
+    fun bindGetFavoritesUseCase(useCase: GetFavoritesUseCaseImpl): GetFavoritesUseCase
 }
