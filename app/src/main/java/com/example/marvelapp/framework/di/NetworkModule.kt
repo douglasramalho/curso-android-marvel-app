1394be8a3b7243ace436fdf0b042cd9aca078ce0
Guilherme Severo <guisevero775@gmail.com>
2022-09-22 18:10:19 -0300

    Cria Modulo de DI para a cama de network

--- app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.example.marvelapp.framework.di
 
+import com.example.core.data.network.interceptor.AuthorizationInterceptor
 import com.example.marvelapp.BuildConfig
 import dagger.Module
 import dagger.Provides
@@ -9,11 +10,15 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private const val READ_TIMEOUT = 15L
+    private const val CONNECT_TIMEOUT = 15L
 
     @Provides
     fun provideLoggingInterceptor() =
@@ -25,12 +30,21 @@ object NetworkModule {
         }
 
     @Provides
+    fun provideAuthorizationInterceptor() = AuthorizationInterceptor(
+        publicKey = BuildConfig.PUBLIC_KEY,
+        privateKey = BuildConfig.PRIVATE_KEY,
+        calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    )
+
+    @Provides
     fun provideOkHttpClient(
-        loggingInterceptor: HttpLoggingInterceptor
+        loggingInterceptor: HttpLoggingInterceptor,
+        authorizationInterceptor: AuthorizationInterceptor
     ) = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
-        .readTimeout(15, TimeUnit.SECONDS)
-        .connectTimeout(15, TimeUnit.SECONDS)
+        .addInterceptor(authorizationInterceptor)
+        .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+        .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
         .build()
 
     @Provides
