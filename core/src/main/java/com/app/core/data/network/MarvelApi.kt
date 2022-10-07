906a1e6fadb37d918785f46d148a4f2f826b127c
administrador <lucas_paiva_2009@hotmail.com>
2022-10-07 10:58:25 -0300

    Mapeia endpoint de personagens

---
+++ core/src/main/java/com/app/core/data/network/MarvelApi.kt
@@ -0,0 +1,15 @@
+package com.app.core.data.network
+
+import com.app.core.data.network.response.DataWrapperResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
+interface MarvelApi {
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @QueryMap
+        queries: Map<String, String>
+    ): DataWrapperResponse
+}
