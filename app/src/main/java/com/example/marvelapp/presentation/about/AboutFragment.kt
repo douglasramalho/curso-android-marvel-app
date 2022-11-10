8369cf0c16a4062a3f1b1b466038f571804b4e8d
Eddy <moura@ciandt.com>
2022-11-10 16:46:15 -0300

    Configura navegacao base

---
+++ app/src/main/java/com/example/marvelapp/presentation/about/AboutFragment.kt
@@ -0,0 +1,20 @@
+package com.example.marvelapp.presentation.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.marvelapp.R
+
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -0,0 +1,19 @@
+package com.example.marvelapp.presentation.characters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.marvelapp.R
+
+
+class CharactersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_characters, container, false)
+    }
+}
