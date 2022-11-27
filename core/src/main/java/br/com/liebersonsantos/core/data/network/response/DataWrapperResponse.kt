4db3bc22655a1c3a01eed7e32ae5c6d225d8e9ef
liebersonsantos <liebersonsantos.one@gmail.com>
2022-11-26 23:45:04 -0300

    mapper response

---
+++ core/src/main/java/br/com/liebersonsantos/core/data/network/response/DataWrapperResponse.kt
@@ -0,0 +1,6 @@
+package br.com.liebersonsantos.core.data.network.response
+
+data class DataWrapperResponse(
+    val copyright: String,
+    val data: DataContainerResponse
+)
---
+++ core/src/main/java/br/com/liebersonsantos/core/data/network/response/ThumbnailResponse.kt
@@ -0,0 +1,6 @@
+package br.com.liebersonsantos.core.data.network.response
+
+data class ThumbnailResponse(
+    val path: String,
+    val extension: String
+)
