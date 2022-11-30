783bddbbc70f5f90769f09c56e4cb9728328562d
Lucas Matheus Mendes <lucasreleases@gmail.com>
2022-11-30 16:09:55 -0300

    Project organization optimization

--- app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
@@ -1,4 +1,4 @@
-package com.example.core.data.network.response
+package com.example.marvelapp.framework.network.response
 
 data class DataContainerResponse(
     val results: List<CharacterResponse>
--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -1,19 +1,41 @@
 package com.example.marvelapp.presentation.characters
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.marvelapp.R
+import androidx.fragment.app.Fragment
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding: FragmentCharactersBinding get() = _binding!!
+
+    private val charactersAdapter = CharactersAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ) = FragmentCharactersBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+    }
+
+    private fun initCharactersAdapter(){
+        with(binding.recyclerCharacters){
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
     }
 }
