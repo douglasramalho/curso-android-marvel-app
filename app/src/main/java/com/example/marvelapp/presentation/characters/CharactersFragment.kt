586d8ed01fb02fcb3c11e83092f8e00fe843e5fc
diegodromer <diego.dromer@hotmail.com>
2022-11-30 08:18:47 -0300

    Exibe personagens da api da Marvel.

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.core.domain.model.Character
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CharactersFragment : Fragment() {
 
     private var _binding: FragmentCharactersBinding? = null
     private val binding: FragmentCharactersBinding get() = _binding!!
+
+    private val viewModel: CharactersViewModel by viewModels()
 
     private val charactersAdapter = CharactersAdapter()
 
@@ -29,20 +34,15 @@ class CharactersFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initCharactersAdapter()
 
-        charactersAdapter.submitList(
-            listOf(
-                Character("3-D Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
-                Character("3-D Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
-                Character("3-D Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
-                Character("3-D Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
-                Character("3-D Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
-                Character("3-D Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg")
-            )
-        )
+        lifecycleScope.launch {
+            viewModel.charactersPagingData("").collect { pagingData ->
+                charactersAdapter.submitData(pagingData)
+            }
+        }
     }
 
-    private fun initCharactersAdapter(){
-        with(binding.recyclerCharacter){
+    private fun initCharactersAdapter() {
+        with(binding.recyclerCharacter) {
             setHasFixedSize(true)
             adapter = charactersAdapter
         }
---
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.marvelapp.presentation.characters
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.core.domain.model.Character
+import com.example.core.usecase.GetCharactersUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+class CharactersViewModel @Inject constructor(
+    private val getCharactersUseCase: GetCharactersUseCase
+): ViewModel() {
+
+    fun charactersPagingData(query: String) : Flow<PagingData<Character>> {
+        return getCharactersUseCase(
+            GetCharactersUseCase.GetCharactersParams(query, getPageConfig())
+        ).cachedIn(viewModelScope)
+    }
+
+    private fun getPageConfig() = PagingConfig (
+        pageSize = 20
+    )
+}
