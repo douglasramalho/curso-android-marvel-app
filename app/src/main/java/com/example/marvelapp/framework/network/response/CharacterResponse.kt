a4636a87cf85da02d1af3ff64b5492fecf326da9
Lucas Matheus Mendes <lucasreleases@gmail.com>
2022-12-02 11:32:31 -0300

    Paging source implemented

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,7 +1,16 @@
 package com.example.marvelapp.framework.network.response
 
+import com.example.core.domain.model.Character
+
 data class CharacterResponse(
-    val id : String,
-    val name : String,
+    val id: String,
+    val name: String,
     val thumbnail: ThumbnailResponse
 )
+
+fun CharacterResponse.toCharacterModel(): Character {
+    return Character(
+        name = this.name,
+        imageUrl = "${this.thumbnail.path}.${this.thumbnail.extension}"
+    )
+}
