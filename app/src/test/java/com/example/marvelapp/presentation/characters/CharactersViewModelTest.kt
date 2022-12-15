9a4d1bf7350e7d8fc88d6f93149f687396e89b9f
diegodromer <diego.dromer@hotmail.com>
2022-12-14 23:14:15 -0300

    Corrige teste unitário do view model

--- app/src/test/java/com/example/marvelapp/presentation/characters/CharactersViewModelTest.kt
+++ app/src/test/java/com/example/marvelapp/presentation/characters/CharactersViewModelTest.kt
@@ -8,6 +8,7 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
@@ -48,7 +49,7 @@ class CharactersViewModelTest {
         charactersViewModel = CharactersViewModel(getCharactersUseCase)
     }
 
-    /*@ExperimentalCoroutinesApi #bad
+    @ExperimentalCoroutinesApi
     @Test
     fun `should validate the paging data object values when calling charactersPagingData`() =
         runBlockingTest  {
@@ -62,9 +63,9 @@ class CharactersViewModelTest {
 
             val result = charactersViewModel.charactersPagingData("")
 
-            assertEquals(1, result.count())
-        }
-*/
+            assertNotNull(result.first())
+    }
+
     @ExperimentalCoroutinesApi
     @Test(expected = java.lang.RuntimeException::class)
     fun `should throw an exception when the calling to the use case returns an exception`()
