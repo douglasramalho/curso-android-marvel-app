c019da0603297864d885dace7dd8292e7f2f63d6
Igor Nobre <ferreiranobre@outlook.com>
2022-12-28 22:51:16 -0300

    create networkmodule

--- app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -9,6 +9,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 @Module
 @InstallIn(SingletonComponent::class) //criar uma instancia para toda a aplicação
@@ -23,6 +24,22 @@ object NetworkModule {
                 } else HttpLoggingInterceptor.Level.NONE
             )
         }
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
     }
 
     @Provides
