7874f6168da3810aff059635ffeba0f8acc3a2ba
Igor Nobre <ferreiranobre@outlook.com>
2022-12-28 22:44:06 -0300

    create interceptor

---
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.example.marvelapp.framework.di
+
+import com.example.marvelapp.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class) //criar uma instancia para toda a aplicação
+object NetworkModule {
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else HttpLoggingInterceptor.Level.NONE
+            )
+        }
+    }
+
+    @Provides
+    fun providerRetrofit(
+        okHttpClient: OkHttpClient,
+        converterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+}
