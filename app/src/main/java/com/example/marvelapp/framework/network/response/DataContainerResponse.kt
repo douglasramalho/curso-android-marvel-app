64c11d27008c249cfcd8b076fde1654e743cd239
lieberson <liebersonsantos.one@gmail.com>
2023-02-15 22:44:16 -0300

    init optimization

--- app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
@@ -1,7 +1,12 @@
 package com.example.marvelapp.framework.network.response
 
+import com.google.gson.annotations.SerializedName
+
 data class DataContainerResponse(
+    @SerializedName("offset")
     val offset: Int,
+    @SerializedName("total")
     val total: Int,
+    @SerializedName("results")
     val results: List<CharacterResponse>
 )
