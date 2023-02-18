7146df88895e5f7d56d3fb1c483573bc6985d6f0
lieberson <liebersonsantos.one@gmail.com>
2023-02-18 10:17:45 -0300

    test branch protection

--- app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/favorites/FavoritesFragment.kt
@@ -14,7 +14,6 @@ class FavoritesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 }
