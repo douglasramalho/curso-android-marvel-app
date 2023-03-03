6f6d4f17e65d5e076613a088281944b3018ec02c
lieberson <liebersonsantos.one@gmail.com>
2023-03-03 09:49:34 -0300

    teste esteira

--- app/src/main/java/com/example/marvelapp/presentation/about/AboutFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/about/AboutFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import com.example.marvelapp.R
 
 class AboutFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
