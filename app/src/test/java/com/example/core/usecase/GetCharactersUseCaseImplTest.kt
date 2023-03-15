895b5a053962b98b5f9f578a2ae8e1f710f526cd
Lucas Matheus Mendes <33559835+lucasmathmendes@users.noreply.github.com>
2023-03-15 19:59:48 -0300

    Testing PagingSource

---
+++ app/src/test/java/com/example/core/usecase/GetCharactersUseCaseImplTest.kt
@@ -0,0 +1,57 @@
+package com.example.core.usecase
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingSource
+import com.example.core.data.repository.CharactersRepository
+import com.example.testing.MainCoroutineRule
+import com.example.testing.model.CharacterFactory
+import com.example.testing.pagingsource.PagingSourceFactory
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetCharactersUseCaseImplTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Mock
+    lateinit var repository: CharactersRepository
+
+    private lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    private val hero = CharacterFactory().create(CharacterFactory.Hero.ThreeDMan)
+
+    private val fakePagingSource = PagingSourceFactory().create(listOf(hero))
+
+    @Before
+    fun setUp() {
+        getCharactersUseCase = GetCharactersUseCaseImpl(repository)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should validate flow paging data creation when invoke from use case is called`() =
+        runTest {
+            whenever(repository.getCharacters(""))
+                .thenReturn(fakePagingSource)
+
+            val result = getCharactersUseCase.invoke(GetCharactersUseCase.GetCharactersParams("", PagingConfig(20)))
+
+            verify(repository).getCharacters("")
+
+            assertNotNull(result.first())
+        }
+}
---
+++ app/src/test/java/com/example/marvelapp/framework/paging/CharactersPagingSourceTest.kt
@@ -0,0 +1,73 @@
+package com.example.marvelapp.framework.paging
+
+import androidx.paging.PagingSource
+import com.example.core.data.repository.CharactersRemoteDataSource
+import com.example.marvelapp.factory.response.DataWrapperResponseFactory
+import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.example.testing.MainCoroutineRule
+import com.example.testing.model.CharacterFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class CharactersPagingSourceTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Mock
+    lateinit var remoteDataSource: CharactersRemoteDataSource<DataWrapperResponse>
+
+    private val dataWrapperResponseFactory = DataWrapperResponseFactory()
+    private val characterFactory = CharacterFactory()
+
+    private lateinit var charactersPagingSource: CharactersPagingSource
+
+    @Before
+    fun setUp() {
+        charactersPagingSource = CharactersPagingSource(remoteDataSource, "")
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should return a success load result when load is called`() = runTest {
+        // Arrange
+        whenever(remoteDataSource.fetchCharacters(any()))
+            .thenReturn(dataWrapperResponseFactory.create())
+
+        // Act
+        val result = charactersPagingSource.load(
+            PagingSource.LoadParams.Refresh(
+                null,
+                loadSize = 2,
+                false
+            )
+        )
+
+        //Assert
+        val expected = listOf(
+            characterFactory.create(CharacterFactory.Hero.ThreeDMan),
+            characterFactory.create(CharacterFactory.Hero.ABomb),
+        )
+
+        assertEquals(
+            PagingSource.LoadResult.Page(
+                data = expected,
+                prevKey = null,
+                20
+            ), result
+        )
+
+    }
+
+}
