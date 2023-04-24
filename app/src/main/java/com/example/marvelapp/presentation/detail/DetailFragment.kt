3bd5ddd8a79a5fb26888af288d99c4c19270c830
Lucas Matheus Mendes <33559835+lucasmathmendes@users.noreply.github.com>
2023-04-24 20:55:57 -0300

    Detail Fragment Implemented

---
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailFragment.kt
@@ -0,0 +1,32 @@
+package com.example.marvelapp.presentation.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.marvelapp.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding: FragmentDetailBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentDetailBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
