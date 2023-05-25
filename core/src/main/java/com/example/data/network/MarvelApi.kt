b67aebc441b9b7e2bd5b21ab6bb6035bb4f9d05b
Thiago Nunes Monteiro <thiagomonteiro.trab@outlook.com>
2023-05-24 23:55:05 -0300

    feat: Mapping characters endpoint

---
+++ core/src/main/java/com/example/data/network/MarvelApi.kt
@@ -0,0 +1,16 @@
+package com.example.data.network
+
+import com.example.data.network.response.DataWrapperResponse
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+
+interface MarvelApi {
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @QueryMap
+        queries: Map<String, String>
+    ): DataWrapperResponse
+
+}
