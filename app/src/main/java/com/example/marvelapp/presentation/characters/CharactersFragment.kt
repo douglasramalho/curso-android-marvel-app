5e0037c4adb529f9755bf3287ec2654f3c157c9a
leoprocopio <leonardolp@gmail.com>
2024-05-19 11:25:02 -0300

    Implementando test com Junit4

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -1,19 +1,16 @@
 package com.example.marvelapp.presentation.characters
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.paging.LoadState
-import androidx.transition.Visibility
-import com.example.core.domain.model.Character
-import com.example.marvelapp.R
 import com.example.marvelapp.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
---
+++ app/src/test/java/com/example/marvelapp/presentation/characters/CharactersViewModelTest.kt
@@ -0,0 +1,81 @@
+package com.example.marvelapp.presentation.characters
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.example.core.domain.model.Character
+import com.example.core.usecase.GetCharactersUseCase
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class CharactersViewModelTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private lateinit var charactersViewModel: CharactersViewModel
+
+    @Mock
+    lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        charactersViewModel = CharactersViewModel(getCharactersUseCase)
+    }
+
+    private val pagingDataCharacters = PagingData.from(
+        listOf(
+            Character(
+                "3-D Man",
+                "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+            ),
+            Character(
+                "A-bomb (HAS)",
+                "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16.jpg"
+            )
+        )
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `should validate the paging data object values when calling charactersPagingData`() = runBlockingTest {
+        whenever(getCharactersUseCase.invoke(
+            any()
+//            GetCharactersUseCase.GetCharactersParams("", PagingConfig(20))
+        )).thenReturn(
+            flowOf(pagingDataCharacters)
+        )
+
+        val result = charactersViewModel.charactersPagingData("")
+
+        assert(result.count() == 1)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test(expected = RuntimeException::class)
+    fun `should throw an exception when the calling to the use case returns an exception`() = runBlockingTest {
+        whenever(getCharactersUseCase.invoke(any()))
+            .thenThrow(RuntimeException())
+
+        charactersViewModel.charactersPagingData("")
+    }
+
+    
+
+}
