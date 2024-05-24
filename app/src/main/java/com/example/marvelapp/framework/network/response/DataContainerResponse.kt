257df2c4c3254887706bb8654af411bacfcf61d2
leoprocopio <leonardolp@gmail.com>
2024-05-24 15:22:47 -0300

    Melhorias nos builds.

--- app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
@@ -1,9 +1,13 @@
 package com.example.marvelapp.framework.network.response
 
 import com.example.core.domain.model.Character
+import com.google.gson.annotations.SerializedName
 
 data class DataContainerResponse(
+    @SerializedName("offset")
     val offset: Int,
+    @SerializedName("total")
     val total: Int,
+    @SerializedName("results")
     val results: List<CharacterResponse>
 )
