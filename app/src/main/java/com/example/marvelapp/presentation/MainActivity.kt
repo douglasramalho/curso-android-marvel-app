5d66da94ac27f58d59eb44fa6523f838da28f6fb
danielrn07 <pdaniel.contato@gmail.com>
2025-03-02 16:03:40 -0300

    feat: configure project modules

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.marvelapp
+package com.example.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
