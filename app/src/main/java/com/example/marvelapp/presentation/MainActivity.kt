659e17b543a3031bd65c14068a012c3bef8ef69d
mguib <marcos.silva@softlive.dev>
2026-09-15 10:45:31 -0400

    fix: Adding core modules from repository

---
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.marvelapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.marvelapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
