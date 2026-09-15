b18bece7ec3220f68c08b605aef90732fc4c88e1
mguib <marcos.silva@softlive.dev>
2026-09-15 14:54:03 -0400

    Config Detekt in project

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    id("dev.detekt")
 }
+
+apply(from = rootProject.file("config/detekt/detekt.gradle"))
+
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
